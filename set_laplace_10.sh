928a8d129e142b827d125b60e2beacb53ef5493c
CY Chen <albert.cyc.tw@gmail.com>
2022-02-18 16:13:14 -0800

    Add scripts for setting the scheduling mode for SCHED_DEADLINE

---
+++ set_laplace_10.sh
@@ -0,0 +1,6 @@
+sudo sysctl kernel.sched_dl_mode=2
+sudo sysctl kernel.sched_dl_rad_enabled=0
+sudo sysctl kernel.sched_dl_rad_idle_enabled=0
+sudo sysctl kernel.sched_dl_rad_fg_enabled=0
+sudo sysctl kernel.sched_dl_rad_utr_enabled=0
+sudo sysctl kernel.sched_dl_rad_epsilon=10
---
+++ set_vanilla_edf.sh
@@ -0,0 +1,5 @@
+sudo sysctl kernel.sched_dl_mode=0
+sudo sysctl kernel.sched_dl_rad_enabled=0
+sudo sysctl kernel.sched_dl_rad_idle_enabled=0
+sudo sysctl kernel.sched_dl_rad_fg_enabled=0
+sudo sysctl kernel.sched_dl_rad_utr_enabled=0
